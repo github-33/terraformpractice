68482da1d286032c24191993c193a8c26b0263b6
github-33 <susmitha.maddana@dxc.com>
2021-07-10 08:32:30 +0530

    adding vpc module

---
+++ vpc/main.tf
@@ -0,0 +1,36 @@
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "3.0"
+
+    project_id   = "practice-project-313404"
+    network_name = "terraform-vpc"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-west1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-east4"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        },
+        {
+            subnet_name               = "subnet-03"
+            subnet_ip                 = "10.10.30.0/24"
+            subnet_region             = "us-west2"
+            subnet_flow_logs          = "true"
+            subnet_flow_logs_interval = "INTERVAL_10_MIN"
+            subnet_flow_logs_sampling = 0.7
+            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+        }
+    ]
+
+    
+    
+}
