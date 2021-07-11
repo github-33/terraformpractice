d5133f22cdc91918968f57d7aba11926b2c3693c
github-33 <susmitha.maddana@dxc.com>
2021-07-11 07:57:03 +0530

    network files

---
+++ networks/main.tf
@@ -0,0 +1,12 @@
+module "address" {
+  source       = "terraform-google-modules/address/google"
+  version      = "3.0.0"
+  project_id   = practice-project-313404
+  region       = "europe-west1"
+  address_type = "EXTERNAL"
+  names = [
+    "regional-external-ip-address-1",
+    "regional-external-ip-address-2",
+    "regional-external-ip-address-3"
+  ]
+}
