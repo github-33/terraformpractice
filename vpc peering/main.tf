2a85c97d7a869dbdc02982cdaaf1c6750502d374
github-33 <susmithamaddana3@gmail.com>
2021-07-11 08:40:51 +0530

    vpc peering

---
+++ vpc peering/main.tf
@@ -0,0 +1,7 @@
+module "peering" {
+  source = "terraform-google-modules/network/google//modules/network-peering"
+
+  prefix        = "deafult to peering"
+  local_network = "projects/practice-project-313404/global/networks/default"
+  peer_network  = "projects/practice-project-313404/global/networks/vpc-singapore"
+}
